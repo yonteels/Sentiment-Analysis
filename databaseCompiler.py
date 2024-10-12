b6d7dfd8031dd1f415cbe6a91c6dff39448a460a
Yonteel <leetony0612@gmail.com>
2024-10-12 12:34:55 -0400

    renamed:    animeScrapper.py -> animeReviewScrapper.py 	modified:   databaseCompiler.py 	modified:   sentimentAnalysis.py

--- databaseCompiler.py
+++ databaseCompiler.py
@@ -1,16 +1,17 @@
 import csv
 import pandas
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host ="localhost",
+    user = "admin",
+    password = "admin"
+)
+
+cursor =mydb.cursor()
 
 with open('review.csv', mode = 'r') as file:
     csvFile = csv.DictReader(file)
 
-    #write into mysql or sqlite or something
-    for row in csvFile:
-        print(row['Japanese Title'])
-        print(row['English Title'])
-        print(row['Rating'])
-        print(row['Reviewer'])
-        print(row['Review'])
-        print(row['Reviewers'])
-        print(row['Rating'])
+
 
--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -7,7 +7,11 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 import csv
 
+# read in data from csv
 
+# put data through nltk
+
+# parse data and generate a comprehensive ieaieiai    ........               
 
 
 
